4645a723c6023a30515efadf8f9476704c60d53b
Joona Hartman <joona.hartman@varian.com>
2022-01-27 16:49:34 +0200

    Add load file dialog

---
+++ Client/Shared/LoadFile.cs
@@ -0,0 +1,10 @@
+﻿using W8lessLabs.Blazor.LocalFiles;
+
+namespace DicomWebAssembly;
+
+public class LoadFile
+{
+    public SelectedFile SelectedFile { get; set; }
+    public bool IsLoaded { get; set; }
+    public int LoadedSize { get; set; }
+}
